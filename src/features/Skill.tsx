47f6efdc187d1929f411c9604edcda470282ad2a
jsh <jeonsh024@gmail.com>
2025-05-09 17:02:44 +0900

    feat: skill list update

--- src/features/Skill.tsx
+++ src/features/Skill.tsx
@@ -1,7 +1,27 @@
 'use client'
 
 const Skill = () => {
-  return <div></div>
+  return (
+    <div>
+      <ul>
+        <li>vue.js</li>
+        <li>sass</li>
+        <li>vue-qeury</li>
+        <li>react.js</li>
+        <li>next.js</li>
+        <li>tanstack query</li>
+        <li>react-hook-form</li>
+        <li>javascript</li>
+        <li>typescript</li>
+        <li>tailwind</li>
+        <li>github</li>
+        <li>photoshop</li>
+        <li>sketch</li>
+        <li>figma</li>
+        <li>XD</li>
+      </ul>
+    </div>
+  )
 }
 
 export default Skill
