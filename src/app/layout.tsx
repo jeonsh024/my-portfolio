8e5c44368bf944dddd24db0b5fe0810bba006f36
orderheroemma <108256482+orderheroemma@users.noreply.github.com>
2026-01-05 16:01:47 +0900

    오타수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '전승희 | 프론트렌드 개발자',
+  title: '전승희 | 프론트엔드 개발자',
   description: '전승희 프론트엔드 포트폴리오',
 }
 
